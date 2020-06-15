2f6d21864c1fd703ee0f0388e438aede8e0118f0
kuda <kuda.nyamainashe@ext.soprasteria.com>
2020-06-15 14:55:56 +0100

    Adding our Serverless API

---
+++ notes/create.js
@@ -0,0 +1,23 @@
+import * as uuid from 'uuid';
+
+import handler from "./../libs/handler-lib";
+import dynamoDb from "./../libs/dynamo-lib";
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            userid: event.requestContext.identity.cognitoIdentityId,
+            noteid: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now(),
+            modifiedAt: Date.now(),
+        }
+    };
+
+    await dynamoDb.put(params);
+
+    return params.Item;
+});
